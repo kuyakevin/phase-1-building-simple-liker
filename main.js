df91f6714359dbf9f958ab6ce882bfabea98c668
Kevin Chaney <kevin_chaney@icloud.com>
2022-05-30 20:22:14 -0700

    commit tree 0b08d2c81f99f03352254b5b6aaf5c9a1956d12a

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//three pillars: manipulate the dom, recognize javascript events, response from the server 
+
+modal.classList.add("hidden");
+//Use .hidden to find the element with the id "modal" + global constants 
+
+let likeButton = document.querySelector(".like .like-glyph"); //create a constant for the like button
+//const likeButtons = likeButton.querySelectorAll(".like")
+likeButton.addEventListener('click', () => { //event listener when the heart is clicked
+  mimicServerCall() //callback function 
+  .then(function(serverMessage) { //expect the anonymous function to be completed in the future
+    alert(serverMessage) //display an alert message passing serverMessage as an argument 
+    if(likeButton.innerHTML === EMPTY_HEART) {
+      likeButton.innerHTML = FULL_HEART //change empty herat to full heart
+    }else{
+      likeButton.innerHTML = EMPTY_HEART //otherwise keep as an empty heart
+    }
+    likeButton.classList.toggle("activated-heart"); //change heart color to red
+  })
+    .catch(() => { //catch an error and provide a response  
+      modal.classList.remove("hidden") 
+      modal.innerHTML = "Unknown Server Error. Please Try Again." //remove hidden aspect of modal 
+      setTimeout(() => { //make a timeout function so the error message can dissapear 
+        modal.classList.add('hidden')
+      }, 3000)
+    })
+  })
+
+
 
 
 
